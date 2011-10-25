ad3ce627f525e0d9279e79547c4b3c4695c8a1b6
jean <sjeanc911@gmail.com>
2011-10-25 23:55:12 +0800

    initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PFP::Application.config.secret_token = 'b3a090e93d53d687e54328008a36a11fc29f22a4029c3184b44ecc1508dc0a5f5358e4a8f86539ca46f33adb53e69b30914969f61fa61ce7dffc3b9730acc20a'
